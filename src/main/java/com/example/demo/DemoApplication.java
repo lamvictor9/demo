cf5782289c39844048479ad0630cc502716f2d5d
victor <victor@192.168.0.100>
2020-09-03 22:26:44 +0800

    test2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,14 +16,22 @@ public class DemoApplication {
 	}
 	
 	@RestController
+	@CrossOrigin(value="http://localhost:8080/")
 	public static class Hello {
 
 		@RequestMapping(value = "/hello")
 		public String hello(@RequestParam(value = "name", defaultValue = "World")  String name){
 			System.out.println("222");
-			return "Hello2, " + name;
+			return "Hello7, " + name;
 		}
-	
+
+		
+		@RequestMapping(value = "/hello3")
+		public String hello3(@RequestParam(value = "name", defaultValue = "World")  String name){
+			System.out.println("333");
+			return "Hello3, " + name;
+		}
+		
 	}
 	
 	
